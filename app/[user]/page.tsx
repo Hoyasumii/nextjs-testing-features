648b6bf867ccd53f644031c443473f003a9caf9a
Hoyasumii <alanreisanjo@gmail.com>
2025-01-22 19:00:07 -0300

    Testes

--- app/[user]/page.tsx
+++ app/[user]/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return "page";
+import { cookies } from "next/headers";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ user: string }>;
+}) {
+  const user = (await params).user;
+  const cookieMessage = await (await cookies()).get("message")?.value;
+
+  return (
+    <div>
+      <p>
+        User{" -> "}
+        {user}
+      </p>
+      <p>
+        Message{" -> "}
+        {cookieMessage}
+      </p>
+    </div>
+  );
 }
